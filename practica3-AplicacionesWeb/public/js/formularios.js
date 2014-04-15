05866302cd7e71cc80aa208e79ad20b04d38bdbc
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-04-14 21:23:04 -0500

    Implemented forms.

--- practica3-AplicacionesWeb/public/js/formularios.js
+++ practica3-AplicacionesWeb/public/js/formularios.js
@@ -1,45 +1,27 @@
 $(function() {
   $('#demo-form').submit(function(event) {
     event.preventDefault();
-    console.log("Foo");
-    //validateForm();
+    printData();
   });
-  function validateForm() {
-    var nickname, email, delegacion, cp, alert_message;
-    alert_message = "";
-    $('.alert').addClass('hidden');
-    $('.alert_messages').empty();
 
-    nickname = $('#name').val();
+  function printData() {
+    var name, email, birthday, delegacion, javalevel, zipcode, color, html;
+    name = $('#name').val();
     email = $('#email').val();
+    birthday = $('#birthday').val();
     delegacion = $('#delegacion').val();
-    cp = $('#codigo_postal').val();
+    javalevel = $('#javalevel').val();
+    zipcode = $('#zipcode').val();
+    color = $('#color').val();
 
-    if(nickname === "" ||
-       email === "" ||
-       cp === "") {
-      alert_message += "<li>No todos los campos estan llenos.</li>"
-    }
+    html = "<h3>Resultados</h3><p>Nombre: " + name + "</p>";
+    html += "<p>Correo: " + email + "</p>";
+    html += "<p>Cumpleaños: " + birthday + "</p>";
+    html += "<p>Delegacion: " + delegacion + "</p>";
+    html += "<p>Nivel Java: " + javalevel + "</p>";
+    html += "<p>Codigo Postal: " + zipcode + "</p>";
+    html += "<p>Color: " + color + "</p>";
 
-    if(nickname.length < 3) {
-      alert_message += "<li>El nickname debe tener al menos 3 caracteres.</li>"
-    }
-
-    if(email.length < 3) {
-      alert_message += "<li>El email debe tener al menos 3 caracteres.</li>"
-    }
-
-    if(cp.length !== 5) {
-      alert_message += "<li>El codigo postal debe ser un numero de 5 digitos.</li>"
-    }
-
-    if (alert_message) {
-      $('.alert_messages').html(alert_message)
-      $('.alert-danger').removeClass('hidden');
-    } else {
-      $('.alert-success').removeClass('hidden');
-    }
-
-
+    $('.form-results').html(html);
   }
 });
