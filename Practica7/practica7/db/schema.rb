93cb0bd16f53635fcb3cd475b56c4558e18d5e13
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-05-25 08:28:14 -0500

    Initial rails project for practica7

---
+++ Practica7/practica7/db/schema.rb
@@ -0,0 +1,31 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 0) do
+
+  create_table "usuarios", force: true do |t|
+    t.string  "username",     limit: 25, null: false
+    t.string  "pass",         limit: 30, null: false
+    t.boolean "admin",                   null: false
+    t.string  "email",        limit: 60, null: false
+    t.string  "nombre",       limit: 30, null: false
+    t.string  "a_paterno",    limit: 25, null: false
+    t.string  "a_materno",    limit: 25, null: false
+    t.boolean "sexo",                    null: false
+    t.date    "f_nacimiento",            null: false
+  end
+
+  add_index "usuarios", ["email"], name: "email", unique: true, using: :btree
+  add_index "usuarios", ["username"], name: "username", unique: true, using: :btree
+
+end
